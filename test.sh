8958cbacd3c95826cc7727938e000a6208706a3e
Ivan Mincik <ivan.mincik@gmail.com>
2025-04-30 14:01:46 +0200

    Tests update

--- test.sh
+++ test.sh
@@ -2,8 +2,13 @@
 
 set -Eeuo pipefail
 
+echo -e "\nTest --app option ..."
 ./app-shell.bash --apps gdal,pdal -- gdalinfo --version | grep "GDAL"
-./app-shell.bash --apps gdal,pdal,python3 --python-packages python3Packages.numpy,python3Packages.pyproj -- python3 --version | grep "Python"
+
+echo -e "\nTest --python-packages option ..."
+./app-shell.bash --apps python3 --python-packages python3Packages.numpy,python3Packages.pyproj -- 'python3 -c "import pyproj; print(pyproj)"' | grep "module 'pyproj'"
+
+echo -e "\nTest --libs option ..."
 ./app-shell.bash --libs stdenv.cc.cc,libz -- env | grep LD_LIBRARY_PATH  | grep "gcc"
 
-echo "DONE: SUCCESS"
+echo -e "\nDONE: SUCCESS"
